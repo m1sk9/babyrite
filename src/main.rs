dedbdd90a3ddd0b1fb7a4ceb6bcd8315cfb67539
Sho Sakuma <me@m1sk9.dev>
2023-10-27 01:01:02 +0900

    refactor: 環境変数のロジックのリファクタリング (#31)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::time::Duration;
 
 use anyhow::Ok;
+use dotenvy::dotenv;
+use env::{BabyriteEnv, BABYRITE_ENV};
 
-use crate::{
-    client::discord_client,
-    env::{env_var, load_dotenv},
-};
+use crate::client::discord_client;
 
 mod adapters;
 mod client;
@@ -19,12 +18,15 @@ pub static VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    load_dotenv();
+    dotenv().ok();
     tracing_subscriber::fmt().compact().init();
 
-    let token = env_var("DISCORD_API_TOKEN");
+    // 環境変数の読み込み. 読み込み後は [&BABYRITE_ENV.get().unwrap().<key>] でアクセスできる.
+    BABYRITE_ENV
+        .set(envy::from_env::<BabyriteEnv>().expect("環境変数の読み込みに失敗しました."))
+        .unwrap();
 
-    discord_client(token).await?;
+    discord_client(&BABYRITE_ENV.get().unwrap().discord_api_token).await?;
 
     Ok(())
 }
