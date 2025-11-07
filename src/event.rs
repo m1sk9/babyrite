729262544e9fe4d74e729ce60390ac0b5ce22eb3
Sho Sakuma <me@m1sk9.dev>
2025-11-08 02:31:22 +0900

    feat: Replace builder

--- src/event.rs
+++ src/event.rs
@@ -1,2 +1,81 @@
-pub mod preview;
-pub mod ready;
+use crate::preview::{MessageLinkIDs, Preview};
+use serenity::all::{ActivityData, Context, EventHandler, Message, Ready};
+use serenity_builder::model::{
+    embed::SerenityEmbed,
+    message::{SerenityMessage, SerenityMessageMentionType},
+};
+
+pub struct BabyriteEventHandler;
+
+#[serenity::async_trait]
+impl EventHandler for BabyriteEventHandler {
+    async fn ready(&self, ctx: Context, bot: Ready) {
+        ctx.set_activity(
+            ActivityData::custom(format!("Running v{}", env!("CARGO_PKG_VERSION"))).into(),
+        );
+        tracing::info!("{} is connected!", bot.user.name);
+    }
+
+    async fn message(&self, ctx: Context, request: Message) {
+        if request.author.bot {
+            return;
+        }
+
+        let Some(ids) = MessageLinkIDs::parse(&request.content) else {
+            return;
+        };
+
+        tracing::info!(
+            "Begin generating the preview. (Requester: {})",
+            &request.author.name
+        );
+
+        let preview = match Preview::get(ids, &ctx).await {
+            Ok(p) => p,
+            Err(e) => {
+                tracing::error!(?e);
+                return;
+            }
+        };
+
+        let (message, channel) = (preview.message, preview.channel);
+        let embed = SerenityEmbed::builder()
+            .description(message.content)
+            .author_name(message.author.name.clone())
+            .author_icon_url(message.author.avatar_url().unwrap_or_default())
+            .footer_text(channel.name)
+            .timestamp(message.timestamp)
+            .color(0x7A4AFFu32)
+            .image_url(
+                message
+                    .attachments
+                    .first()
+                    .map(|a| a.url.clone())
+                    .unwrap_or_default(),
+            )
+            .build();
+
+        let request_channel_id = request.channel_id;
+        let message_builder = SerenityMessage::builder()
+            .embeds(vec![embed])
+            .mention_type(SerenityMessageMentionType::Reply(Box::new(request)))
+            .build();
+
+        let converted_message = match message_builder.convert() {
+            Ok(m) => m,
+            Err(e) => {
+                tracing::error!(?e);
+                return;
+            }
+        };
+
+        if let Err(e) = request_channel_id
+            .send_message(&ctx.http, converted_message)
+            .await
+        {
+            tracing::error!("Failed to send preview: {:?}", e);
+        }
+
+        tracing::info!("Preview sent successfully.")
+    }
+}
---
+++ src/preview.rs
@@ -0,0 +1,93 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
+use serenity::all::{ChannelId, ChannelType, Context, GuildChannel, GuildId, Message, MessageId};
+use url::Url;
+
+use crate::{cache::CacheArgs, config::BabyriteConfig};
+
+pub static MESSAGE_LINK_REGEX: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(r"https://(?:ptb\.|canary\.)?discord\.com/channels/(\d+)/(\d+)/(\d+)").unwrap()
+});
+
+#[derive(serde::Deserialize, Debug)]
+pub struct MessageLinkIDs {
+    pub guild_id: GuildId,
+    pub channel_id: ChannelId,
+    pub message_id: MessageId,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Preview {
+    pub message: Message,
+    pub channel: GuildChannel,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum PreviewError {
+    #[error("Failed to retrieve from cache.")]
+    Cache,
+    #[error("NSFW content previews are not permitted, but the channel is marked as NSFW.")]
+    Nsfw,
+    #[error("The channel is a private channel or private thread.")]
+    Permission,
+    #[allow(clippy::enum_variant_names)]
+    #[error(transparent)]
+    SerenityError(#[from] serenity::Error),
+}
+
+impl MessageLinkIDs {
+    pub fn parse(text: &str) -> Option<MessageLinkIDs> {
+        if !MESSAGE_LINK_REGEX.is_match(text) {
+            return None;
+        }
+
+        match MESSAGE_LINK_REGEX.captures(text) {
+            Some(captures) => {
+                let url = Url::parse(captures.get(0)?.as_str()).ok()?;
+
+                if !matches!(
+                    url.domain(),
+                    Some("discord.com") | Some("canary.discord.com") | Some("ptb.discord.com")
+                ) {
+                    return None;
+                }
+
+                let guild_id = GuildId::new(captures.get(1)?.as_str().parse().ok()?);
+                let channel_id = ChannelId::new(captures.get(2)?.as_str().parse().ok()?);
+                let message_id = MessageId::new(captures.get(3)?.as_str().parse().ok()?);
+
+                Some(MessageLinkIDs {
+                    guild_id,
+                    channel_id,
+                    message_id,
+                })
+            }
+            _ => None,
+        }
+    }
+}
+
+impl Preview {
+    pub async fn get(args: MessageLinkIDs, ctx: &Context) -> Result<Preview, PreviewError> {
+        let caches = CacheArgs {
+            guild_id: args.guild_id,
+            channel_id: args.channel_id,
+        };
+
+        let channel = caches.get(ctx).await.map_err(|_| PreviewError::Cache)?;
+
+        if channel.nsfw && !BabyriteConfig::get().allow_nsfw {
+            return Err(PreviewError::Nsfw);
+        }
+
+        if matches!(
+            channel.kind,
+            ChannelType::Private | ChannelType::PrivateThread
+        ) {
+            return Err(PreviewError::Permission);
+        }
+
+        let message = channel.message(&ctx.http, args.message_id).await?;
+        Ok(Preview { message, channel })
+    }
+}
